41f7173eb3250c7eef83360ce5c408f59b6b8abf
brenoo2018 <thaynanbreno@gmail.com>
2020-07-20 16:08:33 -0300

    add axios and request github api

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Keyboard} from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../services/api';
+
 import {Container, Form, Input, SubmitButton} from './styles';
 
 const Main = () => {
+  const [user, setUser] = useState('');
+
+  async function handleSubmit() {
+    const response = await api.get(`/users/${user}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    setUser((prev) => {
+      [...prev, data];
+    });
+
+    Keyboard.dismiss();
+  }
   return (
     <Container>
       <Form>
@@ -11,8 +33,12 @@ const Main = () => {
           autoCorrect={false}
           autoCaptalize="none"
           placeholder="Adicionar Usuário"
+          returnKeyType="send"
+          onSubmitEditing={handleSubmit}
+          value={user}
+          onChangeText={(text) => setUser(text)}
         />
-        <SubmitButton>
+        <SubmitButton onPress={handleSubmit}>
           <Icon name="add" size={20} color="#FFF" />
         </SubmitButton>
       </Form>
